ad1f97a1623c5c41a0db40a1def6d3fb8548a37d
me-no-dev <me-no-dev@users.noreply.github.com>
2024-04-18 01:58:21 +0000

    IDF release/v5.1 c432c692fa

--- esp32/platformio-build.py
+++ esp32/platformio-build.py
@@ -381,7 +381,7 @@ env.Append(
 
     CPPDEFINES=[
         "ESP_PLATFORM",
-        ("IDF_VER", '\\"v5.1.3-416-gd23b7a0361-dirty\\"'),
+        ("IDF_VER", '\\"v5.1.3-462-gc432c692fa-dirty\\"'),
         ("MBEDTLS_CONFIG_FILE", '\\"mbedtls/esp_config.h\\"'),
         ("SOC_MMU_PAGE_SIZE", 'CONFIG_MMU_PAGE_SIZE'),
         "UNITY_INCLUDE_CONFIG_H",
