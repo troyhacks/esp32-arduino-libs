7b015a59844d511b72663a266e5793fb98eecaa1
me-no-dev <me-no-dev@users.noreply.github.com>
2024-08-15 02:15:06 +0000

    IDF release/v5.1 c608177cf9

--- esp32h2/platformio-build.py
+++ esp32h2/platformio-build.py
@@ -374,7 +374,7 @@ env.Append(
 
     CPPDEFINES=[
         "ESP_PLATFORM",
-        ("IDF_VER", '\\"v5.1.4-605-g5c57dfe949-dirty\\"'),
+        ("IDF_VER", '\\"v5.1.4-659-gc608177cf9-dirty\\"'),
         ("MBEDTLS_CONFIG_FILE", '\\"mbedtls/esp_config.h\\"'),
         ("OPENTHREAD_CONFIG_FILE", '\\"openthread-core-esp32x-ftd-config.h\\"'),
         ("OPENTHREAD_FTD", 1),
