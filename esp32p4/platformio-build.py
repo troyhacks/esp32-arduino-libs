e8dd29acc7304629d526f93d17e39800318e58d8
Troy <5659019+troyhacks@users.noreply.github.com>
2024-10-24 07:29:22 -0400

    Assembler flags added

--- esp32p4/platformio-build.py
+++ esp32p4/platformio-build.py
@@ -71,12 +71,16 @@ if "ZIGBEE_MODE_RCP" in flatten_cppdefines:
     )
 
 env.Append(
+env.Append(
     ASFLAGS=[
-        "-march=rv32imc"
+        "-march=rv32imafc_zicsr_zifencei_xesppie",
+        "-mabi=ilp32f"
     ],
 
     ASPPFLAGS=[
-        "-x", "assembler-with-cpp"
+        "-x", "assembler-with-cpp",
+        "-march=rv32imafc_zicsr_zifencei_xesppie",
+        "-mabi=ilp32f"
     ],
 
     CFLAGS=[
